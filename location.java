4709a35b14502803d3952083d7dab61e4afe09f1
AhmadWali04 <ahmadwali2505@gmail.com>
2025-02-16 15:50:13 -0500

    Soft code day 1
    Just the templates on how the objects will look like under the hood

---
+++ location.java
@@ -0,0 +1,46 @@
+public class location {
+    //location object that will pass off the person/Resturaunts longitute and lattitude to the user object
+    //should return those values through some sort of online call to check their coordinates (maybe google maps?)
+
+    /*
+     * Creating our characteristics of the location class
+     */
+    private float longitude;
+    private float lattitude;
+    private float[] location;
+
+    /*
+     * The creation of the location object
+     */
+    public location(float longitude,float lattitude,float[] location){
+        this.longitude = longitude;
+        this.lattitude = lattitude;
+        this.location = location;
+    }
+
+    //getter methods:
+    public float getLattitude(){
+        return this.lattitude;
+    }
+    public float getLongitutde(){
+        return this.longitude;
+    }
+    
+    public float[] getLocation(){
+        return this.location;
+    }
+
+    //setter methods:
+    private void setLattitude(float lattitude){
+        this.lattitude = lattitude;
+    }
+    private void setLongittude(){
+        this.longitude = longitude;
+    }
+
+    private void setLocation(){
+        this.location = new float[]{longitude,lattitude};
+    }
+    //NEED TO DO: create a retreival method that gets their location from their phone or sumn.
+     
+}
--- user.java
+++ user.java
@@ -1,38 +1,118 @@
-class User:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.preferences = {"price_range": None, "radius": None, "cuisine": []}
-        self.location = None
-        self.search_history = []
-        self.favorites = []
-    
-    def update_preferences(self, price_range=None, radius=None, cuisine=None):
-        if price_range:
-            self.preferences["price_range"] = price_range
-        if radius:
-            self.preferences["radius"] = radius
-        if cuisine:
-            self.preferences["cuisine"] = cuisine
+package Backend;
 
-    def set_location(self, latitude, longitude):
-        self.location = (latitude, longitude)
-    
-    def get_preferences(self):
-        return self.preferences
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
-    def add_to_search_history(self, restaurant_name, date):
-        self.search_history.append({"restaurant": restaurant_name, "date": date})
-    
-    def view_search_history(self):
-        return self.search_history
+public class user{
 
-    def clear_search_history(self):
-        self.search_history = []
+    /*
+     * CREATING THE OBJECT CHARACTERISTICS
+     * Contributed By: Ahmad W
+     */
+    private int id;
+    private String name;
+    //super the location somehow
+    private List<String> favourites;
+    private String searchHistory[]; //use an array because lets not store more than like 5 searches
+    private Map<String,Object> preferences;
+    private float maxPay; //variables to go check my preferences hashmap
+    private float radius;
 
-    def save_favorite_restaurants(self, restaurant_name):
-        if restaurant_name not in self.favorites:
-            self.favorites.append(restaurant_name)
+    /*
+     * INITIALIZING THE OBJECT
+     * Contributed By: Ahmad W
+     */
+
+    public user(int id, String name, float radius,String[] searchHistory){
+        this.id = id;
+        this.name = name;
+        this.searchHistory = searchHistory;
+        //creating the hashmap object:
+        this.preferences = new HashMap<>();
+        this.preferences.put("Price Range", null);
+        this.preferences.put("Radius: ", radius);
+        this.preferences.put("Cuisine: ", null);
+    }
+
+    //action methods:
+
+     public List<String> removeFavourites(String resturantName){
+        //loop through the list, if we find where it is we remove it
+        return null;
+     }
+
+     public void addToFavourites(String resturantName){
+        //add a resturaunt to our favourites list,
+     }
+
+     public String[] updateSearchHistory(){
+        //we just move all elements int eh history down 1, check for edge cases like if were fill
+        return this.searchHistory[];
+     }
+     
+     public void addToSearchHistory(String restaurantName) {
+        updateSearchHistory();
+        this.searchHistory[-1] = (restaurantName);
+        //
+    }
+
+    public String[] viewSearchHistory() {
+        return this.searchHistory;
+    }
+
+    public String[] clearSearchHistory() {
+        for(int i = 0; i <=5; i++){
+            this.searchHistory[i] = null;
+        }
+        return this.searchHistory;
+    }
+
+
+     public void updatePreferences(String priceRange, String radius, List<String> cuisine) {
+        if (priceRange != null) {
+            this.preferences.put("price_range", priceRange);
+        }
+        if (radius != null) {
+            this.preferences.put("radius", radius);
+        }
+        if (cuisine != null) {
+            this.preferences.put("cuisine", cuisine);
+        }
+    }
+
+
+    //getter methods:
+    public int getId(){
+        return this.id;
+    }
     
-    def get_favorites(self):
-        return self.favorites
+    public String getName(){
+        return this.name;
+    }
+
+    public List<String> getFavourites(){
+        return this.favourites;
+    }
+
+    public Map<String, Object> getPreferences() {
+        return this.preferences;
+    }
+    
+    //setter methods:
+    public void setID(int id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setMaxPay(float maxPay){
+        this.maxPay=maxPay;
+    }
+
+    public void setRadius(float radius){
+        this.radius = radius;
+    }
+}
