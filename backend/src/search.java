807514e66af3a18ce9ac6e89745ff3f53b40c14b
AhmadWali04 <ahmadwali2505@gmail.com>
2025-03-12 15:38:48 -0400

    Incomplete search class

---
+++ backend/src/search.java
@@ -0,0 +1,7 @@
+package Backend;
+
+public class search {
+    //this class should work to serach up resturaunts for users.
+    //should take in the users location, resturaunts near them, their preferences
+    
+}
