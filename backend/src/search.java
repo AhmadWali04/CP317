a55d3c59469b2038cd027bdf0918f915469d2fc9
nathan-d3350 <doge3350@mylaurier.ca>
2025-03-24 21:22:31 -0400

    Delete backend/src/search.java
    irrelevant

--- backend/src/search.java
+++
@@ -1,7 +0,0 @@
-package Backend;
-
-public class search {
-    //this class should work to serach up resturaunts for users.
-    //should take in the users location, resturaunts near them, their preferences
-    
-}
